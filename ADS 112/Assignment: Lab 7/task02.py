2b3860e58427fa41448464e9f26ffb329bce0bb9
RT-Jeion <rejuwan002@gmail.com>
2026-03-14 16:44:06 +0600

    Yoo

--- ADS 112/Assignment: Lab 7/task02.py
+++ ADS 112/Assignment: Lab 7/task02.py
@@ -1,2 +1,10 @@
 # Create a set of integers and implement  a function that returns the difference between the maximum and minimum values in the set.
 
+def max_min_dif(int_set):
+    int_list = list(int_set)
+    return int_list[-1] - int_list[0]
+
+nums_set = {8,3,4,1,7,2,9,5}
+max_min_value_diff = max_min_dif(nums_set)
+print("Set of Integrts:", nums_set)
+print("Difference between the maximum and minimum values in the set: ",max_min_value_diff)
--- ADS 112/Assignment: Lab 7/task04.py
+++ ADS 112/Assignment: Lab 7/task04.py
@@ -1,1 +1,8 @@
 # Write a program to merge two sets and print the resulting set.
+x = {2,9,8,11}
+y = {10,5,11,6,3}
+print(x)
+print(y)
+for i in x:
+    y.add(i)
+print(y)
