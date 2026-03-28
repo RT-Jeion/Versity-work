9822c28f0bd8411dc67735c8f44b8a05b8b51dba
RT-Jeion <rejuwan002@gmail.com>
2026-03-28 22:50:30 +0600

    refactor: enhance phonebook functionality and improve user prompts

--- ADS 112/Assignment_Lab-07/task03.py
+++ ADS 112/Assignment_Lab-07/task03.py
@@ -1,23 +1,33 @@
 # Implement a phonebook using a dictionary that can also update the phone number of an existing contact.
 
+import json
+
 phonebook = {}
 
 while True:
     print("\nType [1] to add new contact")
     print("Type [2] to modify existing contact")
-    print("Type [3] to quit")
+    print("Type [3] to show all contacts.")
+    print("Type [4] to quit")
     response = input("Enter your response: ")
 
     if response == "1":
         name = input("Enter name: ")
         number = input("Enter the number: ")
         phonebook[name] = number
+
     elif response == "2":
         name = input("Enter an existing contact name: ")
         number = input("Enter new contact number: ")
         phonebook[name] = number
+    
     elif response == "3":
-        print(phonebook)
+        print("All Contacts.")
+        print(json.dumps(phonebook, indent=1))
+
+    elif response == "4":
+        print("System Terminated.")
         break
+
     else:
         print("Invalid choice")
