0c5c594b212a341635bfa9fbddec5126a568e771
RT-Jeion <rejuwan002@gmail.com>
2026-04-11 22:56:41 +0600

    added new folder

---
+++ ADS 112/Assignment_Lab-08/task1.py
@@ -0,0 +1,12 @@
+# Implement a function that checks if a string is a valid email address using regular expressions
+import re
+
+def valid_mail(mail):
+    pattern = r'[a-zA-Z0-9]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2 ,}'
+    return re.findall(pattern, mail)
+
+
+text = "Contact us at support@example.com or sales@company.org"
+
+print(valid_mail(text))
+print(valid_mail("rejuwan@gmail.com"))
